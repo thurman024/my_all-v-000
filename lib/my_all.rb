3bfba121f2279e90075266c827b2e326c872588e
Stephen Thurman <thurman024@gmail.com>
2018-01-25 14:55:48 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,14 @@ require 'pry'
 
 def my_all?(array)
   i = 0
+  block_return_values = []
   while i < array.length
-    yield(array[i])
+    block_return_values << yield(array[i])
     i += 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
   end
 end
